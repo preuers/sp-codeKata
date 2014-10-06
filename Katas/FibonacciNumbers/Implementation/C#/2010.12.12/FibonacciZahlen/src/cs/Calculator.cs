91e3572182ed059ec29aeb0d2c684dd93ed36415
Stefan Preuer <dummy@localhost>
2014-10-06 20:30:35 +0200

    Added FibonacciNumbers Kata in C#

---
+++ Katas/FibonacciNumbers/Implementation/C#/2010.12.12/FibonacciZahlen/src/cs/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FibonacciZahlen
+{
+    public class Calculator
+    {
+        public static int CalculateFibunacci(int n)
+        {
+            if(n < 0)
+                throw new ArgumentOutOfRangeException("n");
+            
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            return CalculateFibunacci(n - 1) + CalculateFibunacci(n - 2);
+        }
+    }
+}
---
+++ Katas/FibonacciNumbers/Implementation/C#/2010.12.12/FibonacciZahlen/src/test/Test.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace FibonacciZahlen
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestAnchors()
+        {
+            AssertResultForInput(0, 0);
+            AssertResultForInput(1, 1);
+        }
+
+        [Test]
+        public void TestRecursive()
+        {
+            AssertResultForInput(1, 2);
+            AssertResultForInput(2, 3);
+            AssertResultForInput(3, 4);
+            AssertResultForInput(5, 5);
+        }
+
+        private static void AssertResultForInput(int result, int input)
+        {
+            Assert.That(Calculator.CalculateFibunacci(input), Is.EqualTo(result));
+        }
+
+        [Test]
+        public void TestAcceptanceLevel1()
+        {
+            PrintResultFor(6);
+            PrintResultFor(19);
+            PrintResultFor(28);
+            PrintResultFor(36);
+            PrintResultFor(38);
+        }
+
+        private static void PrintResultFor(int n)
+        {
+            Console.WriteLine(Calculator.CalculateFibunacci(n));
+        }
+    }
+}
